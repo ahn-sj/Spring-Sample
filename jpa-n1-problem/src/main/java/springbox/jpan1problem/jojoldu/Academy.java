18c473b963abb3c6c2aa55fb60d579fe17481a71
ahn-sj <jaesa5221@gmail.com>
2023-01-02 23:40:44 +0900

    [N+1] 향로님 포스팅 예제 적용

---
+++ jpa-n1-problem/src/main/java/springbox/jpan1problem/jojoldu/Academy.java
@@ -0,0 +1,37 @@
+package springbox.jpan1problem.jojoldu;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Academy {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "academy_id")
+    private List<Subject> subjects = new ArrayList<>();
+
+    @Builder
+    public Academy(String name, List<Subject> subjects) {
+        this.name = name;
+        if(subjects != null) {
+            this.subjects = subjects;
+        }
+    }
+
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
+        subject.updateAcademy(this);
+    }
+}

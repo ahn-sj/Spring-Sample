eeec6fa9db081a175851b0c6b6549865ebf0e5db
ahn-sj <jaesa5221@gmail.com>
2023-01-04 21:58:24 +0900

    Feat: Add Filter Sample

---
+++ simple-interceptor/src/main/java/spring/sample/simpleinterceptor/config/WebConfig.java
@@ -0,0 +1,22 @@
+package spring.sample.simpleinterceptor.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.sample.simpleinterceptor.filter.LogFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}

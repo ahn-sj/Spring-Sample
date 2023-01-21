3016f091b297a29adf0ce19ee5996035ae8545a9
ahn-sj <jaesa5221@gmail.com>
2023-01-21 14:47:14 +0900

    [Flyway] Begin: Init project

---
+++ flyway-example/README.md
@@ -0,0 +1,4 @@
+# Flyway 적용해보기
+
+## spring initializr
+![img.png](img.png)
---
+++ flyway-example/src/test/java/springbox/flywayexample/FlywayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springbox.flywayexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

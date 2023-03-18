7aec9f6966d5c1c6cebb7e1839ddef0446f3693b
ahn-sj <jaesa5221@gmail.com>
2023-03-18 17:44:16 +0900

    [AOP Annotation] 애너테이션을 활용기 - 인증 체크

---
+++ annotation-sample/src/test/java/springbox/annotationsample/AnnotationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springbox.annotationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
